abcc1a0696f357168867b5469396e96436070db2
Michael Schneider <michael.schneider@profilemedia.ch>
2020-11-11 16:38:37 +0100

    added search field and button, error doesnt work

--- project/js/main.js
+++ project/js/main.js
@@ -4,14 +4,23 @@ $(document).ready(function(){
 
 var key = "caa6fca84a5e70fc39bdd359b1bf7ddb";
 
-function getWeather(){
-  var url = "https://api.openweathermap.org/data/2.5/weather?q=Boston&units=imperial&appid="+key;
+function getWeather(searchQuery){
+  var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=imperial&appid="+key;
+
+    $(".city").text("");
+    $(".temp").text("");
 
   $.ajax(url,{success: function(data){
     console.log(data);
     $(".city").text(data.name);
     $(".temp").text(data.main.temp);
-
+  }, error: function(error){
+    $(".error-message").text("An error occured");
   }})
 
+} 
+
+function searchWeather(){
+  var searchQuery = $(".search").val();
+  getWeather(searchQuery);
 }
